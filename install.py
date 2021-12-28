bfc9f907cf88e21c47e45328df951c68a6525117
ItzHex <62196492+ItzHex08@users.noreply.github.com>
2021-12-28 11:57:19 +0700

    Update install.py

--- install.py
+++ install.py
@@ -19,7 +19,6 @@ def ins():
         try:
                 file_data = open("data/credits.txt")
                 os.system("python2 run.py")
-                break
         except:
                 print( (datetime.now() + timedelta()).strftime(g+'['+w+'%H:%M:%S'+g+']')+" Installing Tools ...")
                 time.sleep(1)
